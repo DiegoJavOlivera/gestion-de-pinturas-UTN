726f9bb0fbb0c4ebbfd394fc97fc8d014a55b489
Diego Olivera <diegolivera93@gmail.com>
2025-06-03 22:51:00 -0300

    nuevocommit

--- js/manejadora.js
+++ js/manejadora.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
         clearTimeout(resizeTimeout);
         resizeTimeout = setTimeout(() => {
             crearTablaPinturas(); 
-        }, 2000); 
+        }, 200); 
     });
 
     document.getElementById("btnAgregar").addEventListener("click", agregarPintura)
